2e714a7fcb16c4b73eb567928cde0de947087f40
Belen Caporale <caporalebelen14@gmail.com>
2023-05-11 08:13:17 -0300

    Se agregaron fuentes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ export default function App() {
   };
   const Content = () => (userNumber ? <PantallaInicio /> : <PantallaUno onIngresar={onIngresar} />);
 
+  const [loaded] = useFonts({
+    'Quicksand-Regular': require('../assets/Fonts/Quicksand-Regular.ttf'),
+    'Quicksand-Light': require('../assets/Fonts/Quicksand-Light.ttf'),
+    'Quicksand-Medium': require('../assets/Fonts/Quicksand-Medium.ttf'),
+    'Quicksand-Bold': require('../assets/Fonts/Quicksand-Bold.ttf'),
+    'Quicksand-SemiBold': require('../assets/Fonts/Quicksand-SemiBold.ttf'),
+  });
+
+  //Si no cargan las fuentes se muestra el Activity Indicator
+  if (!loaded) {
+    return <ActivityIndicator size="large" color={theme.colors.black} />;
+  }
+
   return (
     <View style={styles.container}>
       <Content />
